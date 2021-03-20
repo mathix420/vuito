61f0963fe898587a4011aa6773da3e8c002aa39f
Arnaud Gissinger <agissing@student.42.fr>
2021-03-20 19:52:44 +0100

    [UPDATE] imports

--- src/validators/index.ts
+++ src/validators/index.ts
@@ -1,3 +1,7 @@
-import minLength from './minLength';
-
-export { minLength };
+export { default as maxLength } from './maxLength';
+export { default as minLength } from './minLength';
+export { default as maxValue } from './maxValue';
+export { default as minValue } from './minValue';
+export { default as onlyText } from './onlyText';
+export { default as required } from './required';
+export { default as regex } from './regex';
