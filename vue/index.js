e990ccbd24567b0de2582a3f5d00d5dbe95b51a0
Arnaud Gissinger <agissing@student.42.fr>
2021-03-21 15:30:42 +0100

    [UPDATE] convert vue mixin to es5

--- vue/index.js
+++ vue/index.js
@@ -1,27 +1,29 @@
-export default (validator) => ({
-  data() {
-    return {
-      errors: {},
-      fields: {},
-    };
-  },
-  methods: {
-    async onInput(event) {
-      await validator[event.target.id]
-        .check(event.target.value)
-        .then(() => delete this.errors[event.target.id])
-        .catch((err) => (this.errors[event.target.id] = err));
+module.exports = function (validator) {
+  return {
+    data() {
+      return {
+        errors: {},
+        fields: {},
+      };
     },
-  },
-  watch: {
-    fields: {
-      deep: true,
-      handler() {
-        this.$emit('update', {
-          fields: this.fields,
-          errors: this.errors,
-        });
+    methods: {
+      async onInput(event) {
+        await validator[event.target.id]
+          .check(event.target.value)
+          .then(() => delete this.errors[event.target.id])
+          .catch((err) => (this.errors[event.target.id] = err));
       },
     },
-  },
-});
+    watch: {
+      fields: {
+        deep: true,
+        handler() {
+          this.$emit('update', {
+            fields: this.fields,
+            errors: this.errors,
+          });
+        },
+      },
+    },
+  };
+};
