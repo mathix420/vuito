198b081a058b86dcde108a751571a181cbfb3f68
Arnaud Gissinger <agissing@student.42.fr>
2021-03-21 12:32:40 +0100

    [ADD] @vuito/vue

---
+++ vue/index.js
@@ -0,0 +1,27 @@
+export default (validator) => ({
+  data() {
+    return {
+      errors: {},
+      fields: {},
+    };
+  },
+  methods: {
+    async onInput(event) {
+      await validator[event.target.id]
+        .check(event.target.value)
+        .then(() => delete this.errors[event.target.id])
+        .catch((err) => (this.errors[event.target.id] = err));
+    },
+  },
+  watch: {
+    fields: {
+      deep: true,
+      handler() {
+        this.$emit('update', {
+          fields: this.fields,
+          errors: this.errors,
+        });
+      },
+    },
+  },
+});
