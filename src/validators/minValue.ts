ff659f1264088eb16a30df3c4467476c3356feea
Arnaud Gissinger <agissing@student.42.fr>
2021-05-09 14:17:44 +0200

    [FIX] github publish action

--- src/validators/minValue.ts
+++ src/validators/minValue.ts
@@ -1,5 +1,5 @@
 import { VRow } from '../types';
 
 export default function (min: number): VRow['test'] {
-  return (value: string) => parseInt(value, 10) >= min;
+  return (value: string) => min <= parseInt(value, 10);
 }
