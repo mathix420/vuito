8f9163267ce1bf0557aa2c3db77b320573a2e530
Arnaud Gissinger <agissing@student.42.fr>
2022-05-30 10:30:28 +0200

    [UPDATE] better syntax

--- packages/vuito/src/template.ts
+++ packages/vuito/src/template.ts
@@ -7,11 +7,11 @@ export class Template {
     // eslint-disable-next-line @typescript-eslint/no-this-alias
     const self = this;
     // Map each fields checker in template
-    Object.entries(template as VTemplate).map(([key, tests]) => {
+    for (const [key, tests] of Object.entries(template as VTemplate)) {
       // Add `check` method to all fields checkers
-      tests.check = function (value, parent) {
+      (tests as VTemplateRow).check = function (value, parent) {
         return new Promise(async (resolve, reject) => {
-          await this.map(({ message, test, onlyIf }) => {
+          for (const { message, test, onlyIf } of this) {
             // Conditional check
             if (onlyIf && parent && !onlyIf(parent)) return;
 
@@ -21,14 +21,14 @@ export class Template {
 
             // If test fail => we reject with error message
             if (!test(value)) reject(message);
-          });
+          }
           // All tests passed
           resolve();
         });
       };
 
-      self[key] = tests;
-    });
+      self[key] = tests as VTemplateRow;
+    }
   }
 
   /**
@@ -36,13 +36,13 @@ export class Template {
    */
   public check(object: Record<string, unknown>): Promise<void> {
     return new Promise(async (resolve, reject) => {
-      await Object.entries(this).map(([key, tests]) => {
+      for (const [key, tests] of Object.entries(this)) {
         // Skip `check` method in the template
         // if (key === 'check') return;
 
         // Perform all sub tests, if any fail, the exit with the error message
         (tests as VTemplateRow).check(object[key], object).catch(reject);
-      });
+      }
 
       // All tests passed
       resolve();
