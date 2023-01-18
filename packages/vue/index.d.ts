5ed5fcec05296a0016c469234112b0f6f11b8662
Arnaud Gissinger <agissing@student.42.fr>
2023-01-18 18:09:26 +0100

    [FIX] stupid mistake (leaving console.log ..)
    - also removed support for old syntax

--- packages/vue/index.d.ts
+++ packages/vue/index.d.ts
@@ -1,4 +1,4 @@
-import { Template } from 'vuito/lib/template';
+import type { VTemplate } from 'vuito/lib/vuito';
 import { VueConstructor } from 'vue';
 
-export default function (validator: Template): VueConstructor;
+export default function (validator: VTemplate): VueConstructor;
--- packages/vuito/src/template.ts
+++ packages/vuito/src/template.ts
@@ -11,11 +11,8 @@ function getCheckRowFct(tests: VRow[]): VTemplateRow['check'] {
         value = value.trim();
       }
 
-      console.log(value, message, parent, onlyIf && parent && onlyIf(parent))
-
       // If test fail => we reject with error message
       if (!test(value)) reject(message);
-      console.log(value, 'out')
     }
     // All tests passed
     resolve();
@@ -56,59 +53,4 @@ export function templateify(template: VInputTemplate): VTemplate {
     ...injectedTemplate,
     check: getGlobalChecker(injectedTemplate)
   } as VTemplate
-}
-
-
-/**
- * @deprecated Use templateify method instead
- */
-export class Template {
-  [key: string]: VTemplateRow | VTemplate['check'];
-
-  constructor(template: VInputTemplate) {
-    // eslint-disable-next-line @typescript-eslint/no-this-alias
-    const self = this;
-    console.warn('[WARN:vuito:deprecated] Use templateify method instead.')
-    // Map each fields checker in template
-    for (const [key, tests] of Object.entries(template as VTemplate)) {
-      // Add `check` method to all fields checkers
-      (tests as VTemplateRow).check = function (value, parent) {
-        return new Promise((resolve, reject) => {
-          for (const { message, test, onlyIf } of this) {
-            // Conditional check
-            if (onlyIf && parent && !onlyIf(parent)) continue;
-
-            if (value && typeof value === 'string') {
-              value = value.trim();
-            }
-
-            // If test fail => we reject with error message
-            if (!test(value)) reject(message);
-          }
-          // All tests passed
-          resolve();
-        });
-      };
-
-      self[key] = tests as VTemplateRow;
-    }
-  }
-
-  /**
-   * Global checker
-   */
-  public check(object: Record<string, unknown>): Promise<void> {
-    return new Promise(async (resolve, reject) => {
-      for (const [key, tests] of Object.entries(this)) {
-        // Skip `check` method in the template
-        // if (key === 'check') return;
-
-        // Perform all sub tests, if any fail, the exit with the error message
-        await (tests as VTemplateRow).check(object[key], object).catch(reject);
-      }
-
-      // All tests passed
-      resolve();
-    });
-  }
 }
